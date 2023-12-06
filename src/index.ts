f0db8f97a4b94912d7b72c7f16312b5e3dd18248
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-12-06 21:28:35 +0800

    fix: clean up on unload

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import { settingsConfig } from './settings'
 import { openPopup } from './popup'
 
 function injectMathLive() {
-  if (window.top === null) return
   const script = parent.document.createElement('script')
   script.setAttribute('src', `${logseq.baseInfo.lsr}dist/parent.js`)
   parent.document.body.appendChild(script)
+  return () => parent.document.body.removeChild(script)
 }
 
 function main() {
-  injectMathLive()
+  const removeMathlive = injectMathLive()
   logseq.provideStyle(styleCSS)
   logseq.useSettingsSchema(settingsConfig)
   logseq.Editor.registerSlashCommand('math', async (event) => {
@@ -27,42 +27,46 @@ function main() {
       await openPopup(block.uuid)
     },
   )
-  if (logseq.settings?.selectEdit) {
-    logseq.Editor.onInputSelectionEnd(async (event) => {
-      if (logseq.settings?.selectModifier !== '<none>') {
-        const pressedKeys = parent.document.body.dataset.activeKeystroke?.split('+')
-        const requiredKeys = logseq.settings?.selectModifier.split('+')
-        if (pressedKeys === undefined || requiredKeys === undefined) return
-        if (requiredKeys.some((k: string) => !pressedKeys.includes(k))) {
-          return
-        }
+  logseq.Editor.onInputSelectionEnd(async (event) => {
+    if (logseq.settings?.disabled || !logseq.settings?.selectEdit) return
+    if (logseq.settings?.selectModifier !== '<none>') {
+      const pressedKeys = parent.document.body.dataset.activeKeystroke?.split('+')
+      const requiredKeys = logseq.settings?.selectModifier.split('+')
+      if (pressedKeys === undefined || requiredKeys === undefined) return
+      if (requiredKeys.some((k: string) => !pressedKeys.includes(k))) {
+        return
       }
-      if (event.text.length <= 2) return
-      if (!event.text.match(/^(\$+)([^$]+)\1$/)) return
-      const block = await logseq.Editor.getCurrentBlock()
-      if (block === null) return
-      openPopup(block.uuid, {
-        selectionStart: event.start,
-        selectionEnd: event.end,
-      })
+    }
+    if (event.text.length <= 2) return
+    if (!event.text.match(/^(\$+)([^$]+)\1$/)) return
+    const block = await logseq.Editor.getCurrentBlock()
+    if (block === null) return
+    openPopup(block.uuid, {
+      selectionStart: event.start,
+      selectionEnd: event.end,
     })
+  })
+
+  const onDollar = async (event: KeyboardEvent) => {
+    if (logseq.settings?.disabled || !logseq.settings?.dollarTrigger || event.key !== '$') return
+    const caret = await logseq.Editor.getEditingCursorPosition()
+    if (caret === null) return
+    // HACK: use textContent from #mock-text for faster response
+    // because block content from getCurrentBlock can be old.
+    // i.e. if typed 'hello $$', it might return 'hello'.
+    const blockContent = parent.document.getElementById('mock-text')?.textContent
+    const block = await logseq.Editor.getCurrentBlock()
+    if (block === null) return
+    if (caret.pos > 1 && blockContent?.charAt(caret.pos - 2) === '$') {
+      openPopup(block.uuid)
+    }
   }
-  if (logseq.settings?.dollarTrigger) {
-    parent.addEventListener('keydown', async (event) => {
-      if (event.key !== '$') return
-      const caret = await logseq.Editor.getEditingCursorPosition()
-      if (caret === null) return
-      // HACK: use textContent from #mock-text for faster response
-      // because block content from getCurrentBlock can be old.
-      // i.e. if typed 'hello $$', it might return 'hello'.
-      const blockContent = parent.document.getElementById('mock-text')?.textContent
-      const block = await logseq.Editor.getCurrentBlock()
-      if (block === null) return
-      if (caret.pos > 1 && blockContent?.charAt(caret.pos - 2) === '$') {
-        openPopup(block.uuid)
-      }
-    })
-  }
+  parent.addEventListener('keydown', onDollar)
+
+  logseq.beforeunload(async () => {
+    parent.removeEventListener('keydown', onDollar)
+    removeMathlive()
+  })
 }
 
 logseq.ready(main).catch(logseq.UI.showMsg)
