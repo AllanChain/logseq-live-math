cebf9cd67ecb6a051ddada4822a34d04bde31eab
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-12-09 12:10:03 +0800

    revert: don't expand macros supported by KaTeX
    This reverts commit 79c5011e703f57bcd36a42b7c58332fffd330313.
    
    Wait until https://github.com/arnog/mathlive/issues/1565

--- src/ml-tweak.ts
+++ src/ml-tweak.ts
@@ -1,31 +1,11 @@
-import type { MathfieldElement, Keybinding, MacroDictionary } from 'mathlive'
-
-// NOTE: This list is not complete. Contributions welcome.
-const KNOWN_MACROS = ['bra', 'ket', 'braket', 'Bra', 'Ket', 'Braket']
+import type { MathfieldElement, Keybinding } from 'mathlive'
 
 export function configureMF(mfe: MathfieldElement) {
   mfe.smartFence = logseq.settings?.smartFence ?? true
   mfe.smartMode = logseq.settings?.smartMode ?? false
   mfe.smartSuperscript = logseq.settings?.smartSuperscript ?? false
-
   // Filter out compute engine related stuffs
   mfe.menuItems = mfe.menuItems.filter((item) => !item.id?.startsWith('ce-'))
-
-  // Don't expand macros supported by KaTeX
-  const macros: MacroDictionary = {}
-  for (const macro in mfe.macros) {
-    if (KNOWN_MACROS.includes(macro)) {
-      const macroDef = mfe.macros[macro]
-      macros[macro] =
-        typeof macroDef === 'string'
-          ? { def: macroDef, expand: false }
-          : { ...macroDef, expand: false }
-    } else {
-      macros[macro] = mfe.macros[macro]
-    }
-  }
-  mfe.macros = macros
-
   try {
     mfe.keybindings = [
       ...mfe.keybindings.filter(
