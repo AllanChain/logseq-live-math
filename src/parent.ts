b64b092f78fdef341806bb15a3d309a74e306e79
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2025-09-28 10:59:33 +0800

    feat: add compute engine to math fields
    Waiting the next release of @cortex-js/compute-engine
    
    Closes #40

--- src/parent.ts
+++ src/parent.ts
@@ -1,4 +1,6 @@
 import { MathfieldElement } from 'mathlive'
+import { ComputeEngine } from '@cortex-js/compute-engine'
 
+MathfieldElement.computeEngine = new ComputeEngine()
 MathfieldElement.fontsDirectory = new URL('css/fonts', location.href).href
 MathfieldElement.soundsDirectory = null
